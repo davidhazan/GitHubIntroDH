c07c658ab03f8baacf15a699e6dc29b995de8923
David Hazan <hdavidit@gmail.com>
2018-11-20 12:59:49 +0200

    first commit

---
+++ Intro-GitHub/Intro-GitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro-GitHub
+//
+//  Created by David Hazan on 20/11/2018.
+//  Copyright © 2018 David Hazan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Intro-GitHub/Intro-GitHubTests/Intro_GitHubTests.swift
@@ -0,0 +1,34 @@
+//
+//  Intro_GitHubTests.swift
+//  Intro-GitHubTests
+//
+//  Created by David Hazan on 20/11/2018.
+//  Copyright © 2018 David Hazan. All rights reserved.
+//
+
+import XCTest
+@testable import Intro_GitHub
+
+class Intro_GitHubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
